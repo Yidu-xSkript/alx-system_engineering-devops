a202e4ed8ed7969cc244f3b31de3e2e0961ab739
Yidu-xSkript <yidu95@gmail.com>
2023-02-09 04:49:09 -0800

    correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,11 +7,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    tasks = []
-    for i in todos:
-        if i.get("completed") is True:
-            tasks.append(i.get("title"))
-
+    tasks = [ta.get("title") for ta in todos if ta.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(tasks), len(todos)))
     [print("\t {}".format(task)) for task in tasks]
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Exports todo list to CSV."""
-import sys
-import requests
 import csv
+import requests
+import sys
 
 if __name__ == "__main__":
     userId = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(userId)).json()
+    username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": userId}).json()
 
-    with open("{}.csv".format(userId), "w", newline="") as todo_csv:
-        csvFormat = csv.writer(todo_csv, quoting=csv.QUOTE_ALL)
-        [csvFormat.writerow(
-            [userId, user.get("username"), t.get("completed"), t.get("title")]
+    with open("{}.csv".format(userId), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [userId, username, t.get("completed"), t.get("title")]
          ) for t in todos]
